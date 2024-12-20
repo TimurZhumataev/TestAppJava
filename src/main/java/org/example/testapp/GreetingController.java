2050c87add9542a622c4994d5e9a5ea020419350
TimurZhumataev <zhumataevtimurr@gmail.com>
2024-12-20 15:22:27 +0600

    created greetController

--- src/main/java/org/example/testapp/GreetingController.java
+++ src/main/java/org/example/testapp/GreetingController.java
@@ -1,4 +1,24 @@
 package org.example.testapp;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/greet")
 public class GreetingController {
+
+    @GetMapping
+    public String greet(@RequestParam("name") String name) {
+        return "Hello " + name;
+    }
+
+    @GetMapping("/full")
+    public String full(@RequestParam("name") String name,
+                       @RequestParam("surname") String surname){
+        return "Hello " + name + " " + surname;
+    }
+
+    @GetMapping("/{name}")
+    public String greet2(@PathVariable String name){
+        return "Hello " + name;
+    }
 }
